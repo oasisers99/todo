bc3cac05530dee6b491d1b4edc0303aead3bcb12
oasisers99 <oasisers99@yahoo.com>
2016-10-26 15:18:16 +1030

    cleaned version

---
+++ app/models/todo.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo',{
+	text : String,
+	done : Boolean
+});
--- server.js
+++ server.js
@@ -9,9 +9,8 @@ var bodyParser = require('body-parser');    // pull information from HTML POST (
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
 // configuration =================
-
-//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
-mongoose.connect('mongodb://localhost:27017/');     // connect to mongoDB database on modulus.io
+var database = require('./config/database');
+mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
@@ -20,70 +19,10 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-// listen (start app with node server.js) ======================================
+// listen (start app with node server.js)
 app.listen(8080);
 console.log("App listening on port 8080");
 
+//load the routes
+require('./app/routes')(app);
 
-// routes ==
-// define model
-var Todo = mongoose.model('Todo',{
-	text : String
-});
-
-// api
-// get all todos
-app.get('/api/todos', function(req, res){
-	// use mongoose to get all todos in the database
-	Todo.find(function(err, todos){
-		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if(err){
-        	res.send(err);
-        }
-
-        res.json(todos); // return all todos
-	});
-});
-
-// create todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
-
-    // create a todo, information comes from AJAX request from Angular
-    Todo.create({
-        text : req.body.text,
-        done : false
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err);
-            res.json(todos);
-        });
-    });
-});
-
-// delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id : req.params.todo_id
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err);
-            res.json(todos);
-        });
-    });
-});
-
-//application
-app.get('*', function(req, res){
-	// load the single view file (angular will handle the page changes on the front-end)
-	res.sendfile('./public/index.html');
-});
